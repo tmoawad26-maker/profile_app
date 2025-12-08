41b932fd9d40d7438efb57d68a7b546b2c96d022
tarek <tarek@gamil.com>
2025-12-08 06:48:02 +0200

    Creating game_friend_app.dart file

---
+++ AndroidStudioProjects/game_friend_app/lib/game_friend_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class GameFriendApp extends StatelessWidget {
+  const GameFriendApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+    );
+  }
+}
---
+++ AndroidStudioProjects/game_friend_app/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:game_friend_app/game_friend_app.dart';
+
+void main() {
+  runApp(const GameFriendApp());
+}
+
+
